ce138a137f7bfa934e0e1de5e4fb9281c1319ed7
Chris Waigl <chris.waigl@gmail.com>
2023-11-10 17:31:44 -0900

    work towards bayesian updates with DD25

--- scripts/generate_TDDcorr.py
+++ scripts/generate_TDDcorr.py
@@ -5,9 +5,10 @@ import pandas as pd
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+PREFIX = "DD25"
 BREAKUPPTH = rutil.BREAKUPPTH
-CLIMPTH = rutil.CLIMPTH
-STATIONDATA = rutil.STATIONDATA
+CLIMPTH = rutil.get_climpath(PREFIX)
+STATIONDATA = rutil.get_stationdata(PREFIX)
 OUTPATH = rutil.PROJPATH / "data/breakupdata/working"
 
 ## Change these as needed 
@@ -29,16 +30,16 @@ def makeDF_from_records(records: list[dict[str]]) -> pd.DataFrame:
     outdf.drop(columns=['dummydate'], inplace=True)
     return outdf
 
-def get_correlationrecords(station_tdd: list[Path], 
+def get_correlationrecords(station_dd: list[Path], 
                            breakupDF: pd.DataFrame,
                            datestr: str,
                            locations: list[str]) -> list:
-        for pth in station_tdd:
-            stationname = pth.stem[:-17]
+        for pth in station_dd:
+            stationname = pth.stem[:(-14-len(PREFIX))]
             logging.debug(f"   working on {stationname}")
             teststationDF = pd.read_csv(pth, skiprows=4, index_col=0)
             breakupDF[stationname] = breakupDF.apply(
-                lambda row: rutil.retrieve_tdd_anomaly_fixed(row, stationname, teststationDF, datestr), axis=1)
+                lambda row: rutil.retrieve_dd_anomaly_fixed(row, stationname, teststationDF, datestr), axis=1)
         returnrec = rutil.calculate_corr(breakupDF, locations)
         return returnrec
 
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     breakup['days_since_march1'] = breakup.apply(lambda row: rutil.datestr2dayssince(row.breakup), axis=1)
     logging.info(f"Read breakup DataFrame, {len(breakup)} lines")
     climatologies = pd.read_csv(CLIMPTH, header=3, index_col=0)
-    station_tdd = sorted(list(STATIONDATA.glob("*.csv")))
+    station_dd = sorted(list(STATIONDATA.glob("*.csv")))
     locations = breakup.siteID.unique()
     logging.info("Successfully read climatologies, stations, and locations.")
 
@@ -59,17 +60,11 @@ if __name__ == '__main__':
         print(f"Working on {datestr}")
         breakup_anomaly_fixed = breakup.copy()
         records[datestr] = get_correlationrecords(
-             station_tdd, breakup_anomaly_fixed, datestr, locations) 
+             station_dd, breakup_anomaly_fixed, datestr, locations) 
     recordsDF = makeDF_from_records(records)
 
-    with open(OUTPATH / "TDD_anomaly_correlations.csv", "w") as dst:
-        dst.write("# Correlations between TDD anomalies each date since April 1 and breakup day \n")
+    with open(OUTPATH / f"{PREFIX}_anomaly_correlations.csv", "w") as dst:
+        dst.write(f"# Correlations between {PREFIX} anomalies each date since April 1 and breakup day \n")
         dst.write("# For all selected sites and stations\n")
         dst.write("# \n")
         recordsDF.to_csv(dst)
-
-
-
-
-
-
