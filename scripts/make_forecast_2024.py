969442608311afe31bc1fe6833c4a5c9ed2e788a
Chris Waigl <chris.waigl@gmail.com>
2024-04-11 13:56:04 -0800

    Initial version of 2024 forecasting script

--- scripts/make_forecast_2024.py
+++ scripts/make_forecast_2024.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import datetime as dt 
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
 import scipy.stats as stats
@@ -14,6 +15,8 @@ prefix = "DD25"
 xs = np.arange(-101, 101)
 year = 2024
 for_ffmpeg = False
+DAILY = True
+PLOTS = False
 
 PROJPATH = Path().resolve().parent
 tdd_anomalycorr = PROJPATH / f"data/breakupdata/derived/{prefix}_anomaly_correlations.csv"
@@ -22,6 +25,7 @@ breakupdata = PROJPATH / 'data/breakupdata/'
 breakuppth = breakupdata / "derived/breakupDate_cleaned.csv"
 stationfolder = PROJPATH / f"data/weatherstations/ACIS/{prefix}/dd_cumul_bystation"
 combinedpath = PROJPATH / 'data/weatherstations/ACIS_combined_DD'
+huctablepath = PROJPATH / "data/breakupdata/derived/breakupDate_mean_std_HUC_augmented.csv"
 outfolder = PROJPATH / f"data/working"
 
 def make_likelihood_DF(breakupDF):
@@ -50,22 +54,25 @@ def make_likelihood_DF(breakupDF):
     return likelihoodDF
 
 if __name__ == '__main__':
+    days_start = 31
+    days_end = 90
+    if DAILY:
+        today = dt.datetime.now().strftime('%Y-%m-%d')
+        days_end = ru.datestr2dayssince(today) + 1
+        days_start = ru.datestr2dayssince(today)
 
-    anomalycorrDF = pd.read_csv(tdd_anomalycorr, header=3, index_col=0)
-    locations = sorted(anomalycorrDF.location.unique())
-
+    huctable = pd.read_csv(huctablepath)
     breakupDF = pd.read_csv(breakuppth, header=3, index_col=0)
     breakupDF['days_since_march1'] = breakupDF.apply(
         lambda row: ru.datestr2dayssince(row.breakup), axis=1)
 
     results = {}
-    for location in locations:
+    for _, item in huctable.iterrows():
+        location = item.siteID
+        river = item.river
+        locality = item.locality
         # clean up location
         print(f"working on {location}")
-        try:
-            location_short = location.split('nr')[1].strip()
-        except IndexError: 
-            location_short = location.split('at')[1].strip()
         # load combined station data
         mean_station = pd.read_csv(combinedpath / f"{prefix}_combined_{location.replace(' ', '_')}.csv", 
             skiprows=3, index_col=0)
@@ -76,7 +83,7 @@ if __name__ == '__main__':
 
         # do calculation and generate plots
         if for_ffmpeg: icount = 1
-        for ii in range (32, 50, 1):
+        for ii in range (31, days_end, 1):
             breakup_avg_model = linear_model.LinearRegression() 
             DF = likelihoodDF[likelihoodDF.forecast_day_past_march1==ii].copy()
             DDval = mean_station[f'{year}'][ii].squeeze()
@@ -94,57 +101,65 @@ if __name__ == '__main__':
             prob_wk2 = pdf[108:115].sum()
             prob_wk3 = pdf[115:122].sum()
             forecastdate = ru.dayssince2date(ii, year)
-            print(mu_0, np.round(mu_0), sigma_0, prob_12, prob_37, prob_wk2, prob_wk3)
-            fig, ax1 = plt.subplots()
-            ax1.plot(xs[:102], pdf[:102], linestyle='dashed', color='grey', linewidth=1)
-            ax1.plot(xs[101:], pdf[101:], color='black')
-            ax1.vlines(0, 0, 0.4, colors='black', linestyles='dotted')
-            # Add most likely +/- 3 days
             mostlikely = int(np.round(mu_0) + 1)
-            ax1.vlines(mu_0, 0, 0.4, colors='grey')
+            forecasteddate = ru.dayssince2date(mostlikely+ii-1, year)
             startidx = max(101, 100 + mostlikely-3)
             endidx = startidx + 6
-            shiftflag = False   # do we have to shift the +/- 3 day window?
-            if  100 + mostlikely-3 < 101:
-                shiftflag = True
-            forecasteddate = ru.dayssince2date(mostlikely+ii-1, year)
             plusminus3daysprob = pdf[startidx:endidx+1].sum()
-            ax1.fill_between(xs[startidx:endidx+1], pdf[startidx:endidx+1], color='tab:purple', alpha=0.5)
-            ax1.text(78-ii, .36, f"Most likely breakup: ", color='tab:purple')
-            ax1.text(78-ii, .34, f"in {mostlikely} d on {forecasteddate}", 
-                     color='tab:purple')
-            if shiftflag: 
-                textsnippet = "7d"
-            else:
-                textsnippet = "±3d"
-            ax1.text(78-ii, .32, f"P({textsnippet}) = {plusminus3daysprob*100:.1f} %", color='tab:purple')
-            ax1.text(78-ii, .29, f"P1-2 = {prob_12*100:.1f} %", color='black')
-            ax1.text(78-ii, .27, f"P3-7 = {prob_37*100:.1f} %", color='black')
-            ax1.text(78-ii, .25, f"P8-14 = {prob_wk2*100:.1f} %", color='black')
-            ax1.text(78-ii, .23, f"P15-21 = {prob_wk3*100:.1f} %", color='black')
-            ax1.set_ylim((0, 0.40))
-            ax1.set_ylabel('probability density')
-            ax1.set_xlabel("Days from forecastday")
-            ax2 = ax1.twinx()
-            color = 'tab:blue'
-            sns.scatterplot(data=DF, x='days_from_then', y='mean_DD', ax=ax2)
-            ax2.scatter(x=[mu_0], y=[mean_station.iloc[ii-1][f"{year}"]], c='r')
-            ax2.set_ylabel('DD25', color=color)
-            ax2.tick_params(axis='y', labelcolor=color)
-            ax2.grid(b=None)
-            plt.title(f"{location_short}, prediction on {forecastdate}")
-            plt.xlim((45-ii, 95-ii))
-            loc = location_short.upper().replace(' ', '_')
-            if not for_ffmpeg:
-                fig.savefig(outfolder / f"{loc}_DD25_{year}_{forecastdate}.png", bbox_inches='tight')
-            else:
-                fig.savefig(outfolder / f"{loc}_DD25_{year}_{icount:02d}.png", bbox_inches='tight')
-                icount += 1 
+            print(mu_0, np.round(mu_0), sigma_0, prob_12, prob_37, prob_wk2, prob_wk3)
+            if PLOTS:
+                fig, ax1 = plt.subplots()
+                ax1.plot(xs[:102], pdf[:102], linestyle='dashed', color='grey', linewidth=1)
+                ax1.plot(xs[101:], pdf[101:], color='black')
+                ax1.vlines(0, 0, 0.4, colors='black', linestyles='dotted')
+                # Add most likely +/- 3 days
+                ax1.vlines(mu_0, 0, 0.4, colors='grey')
+                shiftflag = False   # do we have to shift the +/- 3 day window?
+                if  100 + mostlikely-3 < 101:
+                    shiftflag = True
+                ax1.fill_between(xs[startidx:endidx+1], pdf[startidx:endidx+1], color='tab:purple', alpha=0.5)
+                ax1.text(78-ii, .36, f"Most likely breakup: ", color='tab:purple')
+                ax1.text(78-ii, .34, f"in {mostlikely} d on {forecasteddate}", 
+                        color='tab:purple')
+                if shiftflag: 
+                    textsnippet = "7d"
+                else:
+                    textsnippet = "±3d"
+                ax1.text(78-ii, .32, f"P({textsnippet}) = {plusminus3daysprob*100:.1f} %", color='tab:purple')
+                ax1.text(78-ii, .29, f"P1-2 = {prob_12*100:.1f} %", color='black')
+                ax1.text(78-ii, .27, f"P3-7 = {prob_37*100:.1f} %", color='black')
+                ax1.text(78-ii, .25, f"P8-14 = {prob_wk2*100:.1f} %", color='black')
+                ax1.text(78-ii, .23, f"P15-21 = {prob_wk3*100:.1f} %", color='black')
+                ax1.set_ylim((0, 0.40))
+                ax1.set_ylabel('probability density')
+                ax1.set_xlabel("Days from forecastday")
+                ax2 = ax1.twinx()
+                color = 'tab:blue'
+                sns.scatterplot(data=DF, x='days_from_then', y='mean_DD', ax=ax2)
+                ax2.scatter(x=[mu_0], y=[mean_station.iloc[ii-1][f"{year}"]], c='r')
+                ax2.set_ylabel('DD25', color=color)
+                ax2.tick_params(axis='y', labelcolor=color)
+                ax2.grid(b=None)
+                plt.title(f"{locality} ({river}), prediction on {forecastdate}")
+                plt.xlim((45-ii, 95-ii))
+                loc = locality.upper().replace(' ', '_')
+                outdir = outfolder / f"{river.replace(' ', '_')}"
+                outdir.mkdir(parents=True, exist_ok=True)
+                if not for_ffmpeg:
+                    outfn = (outdir / 
+                            f"{loc}_DD25_{year}_{forecastdate}.png")
+                else:
+                    outfn = (outdir /
+                            f"{loc}_DD25_{year}_{icount:02d}.png")
+                    icount += 1 
+                fig.savefig(outfn, bbox_inches='tight')
             resultrecord = {
-                "location": location_short,
+                "location": locality,
+                "river": river,
                 "forecastdate": forecastdate,
                 "most likely breakup in (days)": mostlikely,
                 "forecasted date": forecasteddate,
+                "average breakup date": item.mean_date,
                 "probability of breakup within ± 3 days around forecasted": plusminus3daysprob,
                 "probability of breakup within 1 or 2 days": prob_12, 
                 "probability of breakup within 3-7 days": prob_37, 
@@ -158,10 +173,10 @@ if __name__ == '__main__':
 
     for forecastdate in results:
         outdf = pd.DataFrame.from_records(results[forecastdate])
+        outdf.sort_values(['river', "average breakup date"], inplace=True)
         outfn = f"daily_report_{forecastdate}.csv"
         with open(outfolder / outfn, 'w') as dst:
-            dst.write(','.join(resultrecord.keys()) + '\n')
-            outdf.to_csv(dst, float_format='%.2f')
+            outdf.to_csv(dst, float_format='%.2f', index=False)
             
 
         
