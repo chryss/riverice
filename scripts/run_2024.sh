864e33b1e5c39b01f6c33612eba9f9a854ef0c73
Chris Waigl <chris.waigl@gmail.com>
2024-04-17 13:09:26 -0800

    More 2024 updates, ML, NBM

--- scripts/run_2024.sh
+++ scripts/run_2024.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ACISDIR="../data/weatherstations/ACIS/stationdata/RFC_new_model"
+OUTDIR="../data/DDforecast_2024"
+CLOUDDIR="/Users/chris/Library/CloudStorage/GoogleDrive-cwaigl@alaska.edu/My\ Drive/Shortcuts/Alaska\ River\ Ice\ Forecasting/Forecasts2024/from_DD25_only"
+
 SCRIPTDIR=`pwd`
 
 conda activate fiweps
@@ -13,3 +16,7 @@ mv ${ACISDIR}/NENANA_MUNICIPAL_AIRPORT_T_max_min_avg_sd_swe.csv ${ACISDIR}/NENAN
 python acis2combinedDD.py
 # run new forecast
 python make_forecast_2024.py 
+
+echo "Copying to Google Drive"
+cd $OUTDIR
+cp -R  * ${CLOUDDIR}
